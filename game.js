24639487c19e79287aee86351fc9084d5b7fc67e
Mark <mark.m.towers@gmail.com>
2017-11-18 16:15:27 +0000

    Creates the Paddles

--- game.js
+++ game.js
@@ -1,43 +1,43 @@
-Crafty.init(500, 500);
-Crafty.background('white');
-
 // Paddles
 // [X, Y, Angle, Colour]
-var center = 250;
-var radius = 100;
+var windowSize = 750;
+var center = windowSize/3;
+var radius = windowSize/3;
 var colours = ['blue', 'red', 'purple', 'green', 'pink', 'gray'];
 
 var paddleThickness = 10;
-var paddleLength = 100;
+var paddleLength = 250;
 
 var playerNum = 6;
 var angleRad = 2*Math.PI/playerNum;
 var angleDeg = 360/playerNum;
-
+var paddles = [];
 
 var number_live_balls = 0;
 var max_balls = 4;
 
+Crafty.init(windowSize, windowSize+100);
+Crafty.background('white');
+
 for (var paddlePos = 0; paddlePos < playerNum; paddlePos++) {
-    var newThing = Crafty.e('Paddle, 2D, DOM, Color');
-    newThing.color(colours[paddlePos]);
-    newThing.attr({
-            x: center + radius*Math.sin(paddlePos*angleRad),
-            y: center + radius*Math.cos(paddlePos*angleRad),
-            w: paddleLength, h: paddleThickness,
-            rotation: -paddlePos*angleDeg
-        });
-    newThing.origin("center")
+    var paddle = Crafty.e('Paddle, 2D, DOM, Color');
+    paddle.color(colours[paddlePos]);
+    paddle.attr({
+        x: center + radius * Math.sin(paddlePos * angleRad),
+        y: center + radius * Math.cos(paddlePos * angleRad),
+        w: paddleLength, h: paddleThickness,
+        rotation: -paddlePos * angleDeg
+    });
+    paddle.origin("center");
 
-    console.log("Paddle X:"+newThing.attr("x")+" Y:"+newThing.attr("y")+" R:"+newThing.attr("rotation")+" C:"+colours[paddlePos]);
-
+    console.log("Paddle X:" + paddle.attr("x") + " Y:" + paddle.attr("y") + " R:" + paddle.attr("rotation") + " C:" + colours[paddlePos]);
 }
 
 function createBall() {
     if (number_live_balls < max_balls) {
         number_live_balls++;
 
-        Crafty.e();
+        var ball = Crafty.e();
     }
 
 }
