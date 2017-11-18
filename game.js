ef59bfb6464340729369bbfd11e317db57a27e87
Mark <mark.m.towers@gmail.com>
2017-11-18 16:10:37 +0000

    Creates the Paddles WORKING

--- game.js
+++ game.js
@@ -1,16 +1,19 @@
-Crafty.init(1000, 1100);
+Crafty.init(500, 500);
 Crafty.background('white');
 
 // Paddles
 // [X, Y, Angle, Colour]
-var center = 750;
+var center = 250;
 var radius = 100;
 var colours = ['blue', 'red', 'purple', 'green', 'pink', 'gray'];
 
-var paddleWidth = 10;
+var paddleThickness = 10;
 var paddleLength = 100;
-var angle = 360/6;
+
 var playerNum = 6;
+var angleRad = 2*Math.PI/playerNum;
+var angleDeg = 360/playerNum;
+
 
 var number_live_balls = 0;
 var max_balls = 4;
@@ -18,16 +21,16 @@ var max_balls = 4;
 for (var paddlePos = 0; paddlePos < playerNum; paddlePos++) {
     var newThing = Crafty.e('Paddle, 2D, DOM, Color');
     newThing.color(colours[paddlePos]);
-    newThing.origin("center");
     newThing.attr({
-            x: center + radius*Math.sin(paddlePos*angle),
-            y: center + radius*Math.cos(paddlePos*angle),
-            w: paddleWidth, h: paddleLength,
-            rotation: paddlePos*angle
+            x: center + radius*Math.sin(paddlePos*angleRad),
+            y: center + radius*Math.cos(paddlePos*angleRad),
+            w: paddleLength, h: paddleThickness,
+            rotation: -paddlePos*angleDeg
         });
+    newThing.origin("center")
 
+    console.log("Paddle X:"+newThing.attr("x")+" Y:"+newThing.attr("y")+" R:"+newThing.attr("rotation")+" C:"+colours[paddlePos]);
 
-    console.log(newThing);
 }
 
 function createBall() {
